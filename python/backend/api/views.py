886bfb36bc3de5a39b350c737d1f16a215af2662
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-10-03 13:55:30 -0500

    Created an Api for uploading files

--- python/backend/api/views.py
+++ python/backend/api/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
-from datetime import datetime
-from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.decorators import action
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
-
-class CMPS420ProjectViewSet(viewsets.ViewSet):
-    # Create your views here.
-    @action(methods=['GET'],  detail=False, name='Get Value from input' )
-    def get_val_from( self, request ):
-
-        input = request.GET[ 'input' ]
-
-        return Response( status=status.HTTP_200_OK,
-                data=f"[{ datetime.now() }] input= { input }, value from Django" )
+from rest_framework.views import APIView
+from .serializers import FileUploadSerializer
+class FileUploadAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = FileUploadSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            # you can access the file like this from serializer
+            # uploaded_file = serializer.validated_data["file"]
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
