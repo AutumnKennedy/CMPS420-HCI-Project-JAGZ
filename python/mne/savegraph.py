7377f5713d277c363f6310fe14576c31d3a9f697
Zoe Klein <zoe.klein@selu.edu>
2023-10-23 01:30:45 -0500

    add comments

--- python/mne/savegraph.py
+++ python/mne/savegraph.py
@@ -2,25 +2,14 @@ import mne
 import matplotlib.pyplot as plt
 import numpy as np
 
+# here we need to define a variable that gets the file using an api request
+
+# this function should take in the file (which we need to get from our api)
+# and plot the data, which will then pop up in an interactive window
 def python_funct(file):
     raw = mne.io.read_raw_fif(file)
     raw.compute_psd(fmax=50).plot(picks='data', exclude='bads')
     raw.plot(duration=1, n_channels=5, block=True)
-
-# get data from location
-# sample_data_folder = mne.datasets.sample.data_path()
-# sample_data_raw_file = (
-#     sample_data_folder / 'MEG' / 'sample' / 'sample_audvis_filt-0-40_raw.fif'
-# )
-
-# read the data in the file
-# raw = mne.io.read_raw_fif(sample_data_raw_file)
-
-# plot the data
-# raw.compute_psd(fmax=50).plot(picks='data', exclude='bads')
-# raw.plot(duration=1, n_channels=5, block=True)
-
-# print(raw)
 
 # start, stop = raw.time_as_index([0,10])
 # data, times = raw[:, start:stop]
@@ -30,4 +19,5 @@ def python_funct(file):
 # save the file as a png
 plt.savefig('spectrogram.png', format='png')
 
+# display the png
 plt.show()
